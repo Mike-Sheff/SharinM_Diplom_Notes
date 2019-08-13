f8fdbc4e497f2a309efa6279c9bf7afa1b063a59
Mike-Sh <mike-sheff@yandex.ru>
2019-08-13 15:38:53 +0300

    Correction of comments from the teacher: 3

--- app/src/main/java/ru/netologia/sharinm_diplom_notes/ComparatorNotes.java
+++ app/src/main/java/ru/netologia/sharinm_diplom_notes/ComparatorNotes.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
 
-class ComparatorNotes implements Comparator {
+class ComparatorNotes implements Comparator<Note> {
     private Context context;
 
     ComparatorNotes(Context context) {
@@ -14,56 +14,45 @@ class ComparatorNotes implements Comparator {
     }
 
     @Override
-    public int compare(Object o1, Object o2) {
-        Note note1 = (Note) o1;
-        Note note2 = (Note) o2;
+    public int compare(Note o1, Note o2) {
 
         Date datedeadline1 = null, datedeadline2 = null, dateUpdate1 = null, dateUpdate2 = null;
-
-        int existenceDateDeadline1 = note1.getDateDeadline().length() != 0 ? 1 : 0;
-        int existenceDateDeadline2 = note2.getDateDeadline().length() != 0 ? 1 : 0;
-
-        if (existenceDateDeadline1 == 1) {
-            try {
-                datedeadline1 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(note1.getDateDeadline());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-        if (existenceDateDeadline2 == 1) {
-            try {
-                datedeadline2 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(note2.getDateDeadline());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
+        int result = 0;
 
         try {
-            dateUpdate1 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(note1.getDateUpdateNote());
-            dateUpdate2 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(note2.getDateUpdateNote());
+            datedeadline1 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(o1.getDateDeadline());
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        if (existenceDateDeadline1 > existenceDateDeadline2) {
-            return -1;
-        } else if (existenceDateDeadline2 > existenceDateDeadline1) {
-            return 1;
-        } else if ((existenceDateDeadline1 == existenceDateDeadline2) && (existenceDateDeadline1 == 1)) {
-            if (datedeadline1.equals(datedeadline2)) {
-                if (dateUpdate1.after(dateUpdate2)) {
-                    return -1;
-                }
-            }
-            if (datedeadline1.before(datedeadline2)) {
-                return -1;
-            }
-        } else if ((existenceDateDeadline1 == existenceDateDeadline2) && (existenceDateDeadline1 == 0)) {
-            if (dateUpdate1.after(dateUpdate2)) {
-                return -1;
-            }
+        try {
+            datedeadline2 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(o2.getDateDeadline());
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
-        return 0;
+        try {
+            dateUpdate1 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(o1.getDateUpdateNote());
+            dateUpdate2 = new SimpleDateFormat(context.getString(R.string.formatDate)).parse(o2.getDateUpdateNote());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if ((datedeadline1 != null) && (datedeadline2 != null)) {
+            result = datedeadline1.compareTo(datedeadline2);
+
+            if (result == 0) {
+                result = dateUpdate2.compareTo(dateUpdate1);
+            }
+        } else if ((datedeadline1 == null) && (datedeadline2 == null)) {
+
+            result = dateUpdate2.compareTo(dateUpdate1);
+        } else if (datedeadline1 == null) {
+            return 1;
+        } else if (datedeadline2 == null) {
+            return -1;
+        }
+        return result;
     }
 }
